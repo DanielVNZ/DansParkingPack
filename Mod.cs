63ef75aeb52f536087d0323fc2dc6c9cdab1a782
DanielVNZ <123446671+DanielVNZ@users.noreply.github.com>
2024-11-08 22:38:20 +1300

    Add project files.

---
+++ Mod.cs
@@ -0,0 +1,38 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+using System.IO;
+using Colossal.UI;
+
+namespace DansParkingPack
+{
+    public class Mod : IMod
+    {
+        //public static ILog log = LogManager.GetLogger($"{nameof(DansRuralPack)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+
+        public static string Name = "Dans Parking with Parks Pack";
+        public static string Version = "1.0.0";
+        public static string Author = "DanielVNZ";
+
+        public static string uiHostName = "danielvnz-dansparkingpack";
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+
+
+
+
+            GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset);
+            UIManager.defaultUISystem.AddHostLocation(uiHostName, Path.Combine(Path.GetDirectoryName(asset.path), "thumbs"), false);
+
+
+        }
+
+        public void OnDispose()
+        {
+
+            UIManager.defaultUISystem.RemoveHostLocation(uiHostName);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+## Dans Rural Services Pack
+My inspiration behind this pack was when I was making small towns around my city, I wanted small local options for services without having massive buildings in a small town, yeah the recycling center is pushing it (Opps).
+
+Hope you Enjoy!
+
+## Added Assets:
+* Rural Police Station (5 police cars, 3 jail cells, low maintenance costs)
+* Rural Medical Centre (2 ambulances, 10 hospital beds, low maintenance costs)
+* Rural Recycling Centre (2 Rubbish/Recycling Trucks, low maintenance costs)
+* Rural Post Office (4 Post vans, low maintenance costs)
+* Rural Welfare Office (Low maintenance costs)
+
+## Credits:
+* Thank you to Konsi for all your help with assets.
+* Thank you to StarQ for assisting with the new asset format.
+
+PDX Mods: https://mods.paradoxplaza.com/mods/79063/Windows
